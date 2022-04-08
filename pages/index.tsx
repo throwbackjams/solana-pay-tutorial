eb60acfcc6b09b21e2fd61d26811d4c78a9062e0
throwbackjams <yli@alumni.nd.edu>
2022-04-08 13:27:49 -0500

    add option between Solana Pay and desktop wallet. Add USDC devnet token link

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,11 +10,18 @@ export default function HomePage() {
   return (
     <div className="flex flex-col gap-8 max-w-4xl items-stretch m-auto pt-24">
       <SiteHeading>Concert Ticket Central</SiteHeading>
-      <div className="basis-1/4">
+      <div className="basis-1/4 flex gap-10" >
         <WalletMultiButton className='!bg-purple-900 hover:scale-105'/>
+      </div>
+      <div className="basis-1/4">
+        <a href="/shop"
+          className = "items-center px-10 font-bold rounded-md py-2 max-w-fit self-center bg-purple-900 text-white hover:scale-105 height-48px">
+          Or Click here to checkout with Solana Pay on Mobile Wallet
+          </a>
       </div>
 
       <Products submitTarget='/checkout' enabled={publicKey !== null} />
+      <a className = "rounded-md bg-white text-left p-4" href= "https://spl-token-faucet.com/?token-name=USDC-Dev" target="_blank">Click here to get USDC devnet test tokens</a>
     </div>
   )
 }
--- pages/shop/index.tsx
+++ pages/shop/index.tsx
@@ -6,6 +6,7 @@ export default function ShopPage() {
         <div className="flex flex-col gap-8 max-w-4xl items-stretch m-auto pt-24">
             <SiteHeading>Concert Ticket Central</SiteHeading>
             <Products submitTarget='./shop/checkout' enabled={true}/>
+            <a className = "rounded-md bg-white text-left p-4" href= "https://spl-token-faucet.com/?token-name=USDC-Dev" target="_blank">Click here to get USDC devnet test tokens</a>
         </div>
     )
 }
